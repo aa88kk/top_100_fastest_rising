33f8746092dfd9644a6db2695f79a8b372b04013
Liu <aa88kk@gmail.com>
2021-01-23 20:59:15 +0800

    test

--- main.go
+++ main.go
@@ -48,22 +48,22 @@ func output(lang string, cnt int, pastDays int) {
 
 	var mdData []byte
 	var desc string
-	mdData = []byte("| Stars | Name | Url | Desc | Topics | Created | \n")
+	mdData = []byte("| Stars | Name | Desc | Created | \n")
 	mdData = append(mdData, []byte("| ----- | ------- | ------------- | ------------- |  ------------- |  ------------- |\n")...)
 
 	for _, repo := range results.Repositories {
 		if repo.Description != nil {
 			desc = strings.Replace(*repo.Description, "|", "\\|", -1)
-			line = fmt.Sprintf("| %d | %s | %s | %q | %q | %v |\n", *repo.StargazersCount, *repo.Name, *repo.HTMLURL, desc, strings.Join(repo.Topics, ","), repo.GetCreatedAt())
+			line = fmt.Sprintf("| %d | [%s](%s) | %q | %v |\n", *repo.StargazersCount, *repo.Name, *repo.HTMLURL, desc, strings.Join(repo.Topics, ","), repo.GetCreatedAt())
 		} else {
-			line = fmt.Sprintf("| %d | %s | %s | \"\"| %q | %v |\n", *repo.StargazersCount, *repo.Name, *repo.HTMLURL, strings.Join(repo.Topics, ","), repo.GetCreatedAt())
+			line = fmt.Sprintf("| %d | [%s](%s) | | %v |\n", *repo.StargazersCount, *repo.Name, *repo.HTMLURL, strings.Join(repo.Topics, ","), repo.GetCreatedAt())
 		}
 		mdData = append(mdData, []byte(line)...)
 	}
 
 	mdData = append(mdData, []byte("\n")...)
 
-	mdFilename := fmt.Sprintf("%s_top_%d_since_%s.md", lang, cnt, since.Format("2006-01-02"))
+	mdFilename := fmt.Sprintf("%s_top_%d_past_%d_days.md", lang, cnt, pastDays)
 	ioutil.WriteFile("./"+mdFilename, mdData, 0622)
 
 }
